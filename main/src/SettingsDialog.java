2e850b633b9f666d28f4126973da58b13e9c5c4a
Zümrüd-ü Anka <osmandurdag@hotmail.com>
2021-02-20 00:23:04 +0300

    degisiklik geri alindi

--- main/src/SettingsDialog.java
+++ main/src/SettingsDialog.java
@@ -6,7 +6,5 @@ public class SettingsDialog {
     public static void main(String[] args) {
     
         System.out.println("asdqwf");
-        System.out.println("ikinci sys");
-        System.out.println("sonuncu ve cok buyuk hatali");
     }
 }
